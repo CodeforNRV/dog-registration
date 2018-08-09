281f90c36b30976e24602d3fd8a3fe1f86cf5490
Matthew Lancellotti <mareoraft@gmail.com>
2018-08-09 19:10:34 -0400

    paypal clean.  this commit combined with previous commit shows clean way to add paypal.  next thing to add is putting the correct fee into paypal tx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import { PayPalConfig, PayPalEnvironment, PayPalIntegrationType } from 'ngx-paypal';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,42 +7,4 @@ import { PayPalConfig, PayPalEnvironment, PayPalIntegrationType } from 'ngx-payp
 })
 export class AppComponent {
   title = 'app';
-
-	public payPalConfig?: PayPalConfig;
-
-	ngOnInit(): void {
-	  this.initConfig();
-	}
-
-	private initConfig(): void {
-	  this.payPalConfig = new PayPalConfig(PayPalIntegrationType.ClientSideREST, PayPalEnvironment.Sandbox,
-		  {
-		    commit: true,
-		    client: {
-		    	// your sandbox key below (ClientID)
-		    	sandbox: 'AWd9JBIV7EIawjvRGY0YfOu80fBlDzukHVXiqsy0-wRuhsSejyqd6GF8pK1WI5bCezCTKUAZ7DgTaViR',
-		      // sandbox: 'ASDkF1xliRvr7ozJerVc9X6QyfvvAiQ3bNzYtgFkqZmaWeX9rVBYeV7F',
-		    },
-		    button: {
-		      label: 'paypal', // can be 'paypal', 'venmo', 'credit', ...
-		    },
-		    onPaymentComplete: (data, actions) => {
-		      console.log('OnPaymentComplete');
-		      alert('congrats!  you are the new owner of a dog tag!');
-		    },
-		    onCancel: (data, actions) => {
-		      console.log('OnCancel');
-		    },
-		    onError: (err) => {
-		      console.log('OnError');
-		    },
-		    transactions: [{
-		      amount: {
-		        currency: 'USD',
-		        total: 9,
-		      },
-		    }],
-			}
-		);
-	}
 }
--- src/app/paypal/paypal.component.ts
+++ src/app/paypal/paypal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PayPalConfig, PayPalEnvironment, PayPalIntegrationType } from 'ngx-paypal';
+
 @Component({
   selector: 'app-paypal',
   templateUrl: './paypal.component.html',
@@ -9,8 +11,41 @@ export class PaypalComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+	public payPalConfig?: PayPalConfig;
 
+	ngOnInit(): void {
+	  this.initConfig();
+	}
+
+	private initConfig(): void {
+	  this.payPalConfig = new PayPalConfig(PayPalIntegrationType.ClientSideREST, PayPalEnvironment.Sandbox,
+		  {
+		    commit: true,
+		    client: {
+					// ClientID api credential of the business paypal account
+					sandbox: 'AWd9JBIV7EIawjvRGY0YfOu80fBlDzukHVXiqsy0-wRuhsSejyqd6GF8pK1WI5bCezCTKUAZ7DgTaViR',
+		    },
+		    button: {
+		      label: 'paypal', // can be 'paypal', 'venmo', 'credit', ...
+		    },
+		    onPaymentComplete: (data, actions) => {
+		      console.log('OnPaymentComplete');
+		      alert('congrats!  you are the new owner of a dog tag!');
+		    },
+		    onCancel: (data, actions) => {
+		      console.log('OnCancel');
+		    },
+		    onError: (err) => {
+		      console.log('OnError');
+		    },
+		    transactions: [{
+		      amount: {
+		        currency: 'USD',
+		        total: 9,
+		      },
+		    }],
+			}
+		);
+	}
 
 }
